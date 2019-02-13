e2b705065b7cafd4a05672b387a58f2970acb54e
odanoburu <bcclaro@gmail.com>
2019-02-13 18:26:54 -0200

    - use in-memory ht during runtime, instead of index file - make, read and write index file

--- sensetion.el
+++ sensetion.el
@@ -1,60 +1,108 @@
 ;;; -*- lexical-binding: t; -*-
 (require 'seq)
 (require 's)
+(require 'f)
 (require 'cl-lib)
 (require 'conllu-parse)
 
 
-(defcustom sensetional-index-directory
-  (expand-file-name "~/.sensetion/")
-  "Directory where index files lie.")
+(defcustom sensetion-index-file
+  (expand-file-name "~/.sensetion-index")
+  "Path to index file.")
 
 
-(defun sensetional--synset-type->indexfile (st)
-  (let ((st->if (cl-map 'vector
-                        (lambda (f) (expand-file-name f sensetional-index-directory))
-                        #("noun.index" "verb.index" "adjective.index" "adverb.index"))))
-    (if st
-        (aref st->if st)
-      (expand-file-name "all.index" sensetional-index-directory))))
+(defcustom sensetion-annotation-dir
+  nil
+  "Path to annotation directory")
 
 
-(defun sensetional-make-index ()
-  (cl-labels
-      ((index-file (id f)
-                   (with-temp-buffer
-                     (insert-file-contents f)
-                     (forward-line 1)
-                     (index-sentence sent-id
-                                     (conllu--parse-sent-at-point))))
-
-       (index-sentence (id sent)
-                       (mapc (apply-partially #'index-token id)
-                             (conllu-sent-tokens sent)))
-
-       (index-token (sent-id tk)
-                    (when (and
-                           ;; is to be annotated?
-                           (equal "un"
-                                  (conllu-token-upos tk))
-                           (or (equal "_" (conllu-token-form tk))
-                               (equal "_" (conllu-token-xpos tk))))
-                      (mapc (lambda (lemma) (index-lemma sent-id
-                                                         (conllu--token-id->string
-                                                          (conllu-token-id tk))
-                                                         lemma))
-                            (s-split "|" (conllu-token-lemma tk)))))
-
-       (index-lemma (sent-id tk-id lemma-str)
-                    (when (equal "_" lemma-str)
-                      (user-error "No lemma at %s %s" sent-id tk-id))
-                    (seq-let (lemma key) (s-split "%" lemma-str)
-                      (let ((line (format "%s %s-%s\n" lemma sent-id tk-id))
-                            (index-file (sensetional--synset-type->indexfile key)))
-                        (append-to-file line nil index-file)))))
-    ;; 
-    (let ((fs (directory-files sensetional-annotation-dir t "\\.conllu$")))
-      (mapc (lambda (f) (index-file (file-name-base f) f)) fs))))
+(defcustom sensetion-annotation-file-type
+  "conllu"
+  "File type (extension) of annotation files.")
 
 
-(provide 'sensetional)
+(defvar sensetion--index nil "Index.")
+
+
+(defun sensetion-annotate (lemma &optional pos)
+  (interactive
+   (list (read-string "Lemma to annotate: ")
+         (completing-read "PoS tag?" '("noun" "n"))))
+  (let ((lemma (if pos
+                   (mapcar (apply-partially #'concat lemma) '("%1" "%2" "%3" "%4"))
+                 (list (concat lemma "%" pos)))))
+    nil))
+
+
+
+(defun sensetion--write-index (index-file index)
+  "Write INDEX to INDEX-FILE."
+  (with-temp-file index-file
+    (prin1 index (current-buffer)))
+  "Index written")
+
+
+(defun sensetion--read-index (index-file)
+  "Read index from INDEX-FILE, and set `sensetion--index'."
+  (with-temp-buffer
+    (insert-file-contents index-file)
+    (goto-char (point-min))             ;is this needed?
+    (setq sensetion--index (read (current-buffer))))
+  "Index read")
+
+
+(defun sensetion-make-index (index-file files)
+  "Read annotated files and build index of lemmas* and their
+positions."
+  (interactive
+   (list sensetion-index-file
+         (directory-files (or sensetion-annotation-dir
+                              (read-file-name "Path to annotation directory: " nil nil t))
+                          t
+                          (concat "\\." sensetion-annotation-file-type "$"))))
+  
+  (let ((index-ht (sensetion--make-index files)))
+    (setq sensetion--index index-ht))
+  "Index made")
+
+
+(defun sensetion--make-index (files)
+  "Read annotated files and build hash-table associating lemmas*
+to where in the files they appear."
+  (let ((index-ht (make-hash-table :size 200000 :test #'equal)))
+    (cl-labels
+        ((run (id f)
+              (let ((sent (conllu--string->sent (f-read-text f))))
+                (index-sent id sent)))
+
+         (index-sent (id sent)
+                     (mapc (apply-partially #'index-token id)
+                           (conllu-sent-tokens sent)))
+
+         (index-token (sent-id tk)
+                      (when (and
+                             ;; is to be annotated?
+                             (equal "un"
+                                    (conllu-token-upos tk))
+                             (or (equal "_" (conllu-token-form tk))
+                                 (equal "_" (conllu-token-xpos tk))))
+                        (mapc (lambda (lemma) (index-lemma sent-id
+                                                           (conllu--token-id->string
+                                                            (conllu-token-id tk))
+                                                           lemma))
+                              (s-split "|" (conllu-token-lemma tk)))))
+
+         (index-lemma (sent-id tk-id lemma)
+                      (when (equal "_" lemma)
+                        (user-error "No lemma at %s %s" sent-id tk-id))
+                      ;; lemmas* might be pure ("love") or have pos
+                      ;; annotation ("love%2"), but we don't care
+                      ;; about it here; when retrieving we gotta take
+                      ;; care of this.
+                      (setf (gethash lemma index-ht) (cons (list sent-id tk-id) (gethash lemma index-ht nil)))))
+      ;; 
+      (mapc (lambda (f) (run (file-name-base f) f)) files)
+      index-ht)))
+
+
+(provide 'sensetion)
