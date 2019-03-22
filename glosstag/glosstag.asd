b9cde0b13ab479a9d87a0be115a9e7437d3cd28e
Alexandre Rademaker <arademaker@gmail.com>
2019-03-22 17:13:01 -0300

    freq of senses

--- glosstag/glosstag.asd
+++ glosstag/glosstag.asd
@@ -3,8 +3,8 @@
 
 (asdf:defsystem #:glosstag
   :serial t
-  :depends-on (#:cxml ;; #:cl-ppcre #:flexi-streams
-                      ;; #:cl-fad :fare-csv
+  :depends-on (#:cxml #:cl-ppcre
+		      ;; #:flexi-streams #:cl-fad :fare-csv
                :alexandria :serapeum)
   :components ((:file "package")
 	       (:file "glosstag" :depends-on ("package"))
--- glosstag/utils.lisp
+++ glosstag/utils.lisp
@@ -2,17 +2,35 @@
 (in-package :glosstag)
 
 (defparameter *db* (make-hash-table :test #'equal))
+(defparameter *db-freq* (make-hash-table :test #'equal))
 
 (defun index (&key (*db* *db*))
-  (dolist (file (directory "/Users/ar/work/glosstag/*.plist"))
+  (dolist (file (directory "/Users/ar/Temp/glosstag/*.plist"))
     (with-open-file (in file)
       (loop for plist = (read in nil nil)
 	    while plist
-	    do (let ((pairs (getf (cdr plist) :KEYS)))
+	    do (let ((pairs (getf plist :keys)))
 		 (dolist (p pairs)
 		   (push (car p) (gethash (cdr p) *db*))))))))
 
-(defun get-lemma (lemma &key (*db* *db*))
-  (gethash lemma *db*))
+
+(defun index-freq (&key (*db-freq* *db-freq*))
+  (dolist (file (directory "/Users/ar/Temp/glosstag/*.plist"))
+    (with-open-file (in file)
+      (loop for plist = (read in nil nil)
+	    while plist
+	    do (dolist (tk (getf plist :tokens))
+		 (if (getf tk :lemma)
+		     (let ((lms (cl-ppcre:split "\\|" (getf tk :lemma))))
+		       (mapcar (lambda (l)
+				 (push (getf plist :id)
+				       (gethash l *db-freq*)))
+			       lms))))))))
+  
+
+(defun get-lemma (lemma &key (db *db*))
+  (gethash lemma db))
+
+
 
 
