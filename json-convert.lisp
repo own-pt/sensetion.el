e552edb44cd2a113aa08d4288590043fd781ad4f
odanoburu <bcclaro@gmail.com>
2019-04-25 18:24:15 -0300

    * convert data to json

---
+++ json-convert.lisp
@@ -0,0 +1,80 @@
+(ql:quickload :yason)
+(ql:quickload :alexandria)
+
+(defun main (in-fp synset-fp)
+  (let ((files (directory (make-pathname :defaults in-fp :name :wild :type "plist"))))
+    (with-open-file (synset-out synset-fp :direction :output :if-does-not-exist :create
+					  :if-exists :supersede)
+      (loop
+	for fp in files
+	do (file->json fp synset-out)))))
+
+(defun file->json (fp synset-out)
+  (with-open-file (in fp)
+    (with-open-file (out (make-pathname :defaults in :type "json")
+			 :direction :output :if-does-not-exist :create :if-exists :supersede)
+      (loop
+	for line = (read-line in nil 'eof)
+	until (eq line 'eof)
+	do (let ((plist (read-from-string line)))
+	     (synset->json plist synset-out)
+	     (sentence->json plist out))))))
+
+(defun synset->json (plist out)
+  (let ((ofs (read-from-string (getf plist :ofs)))
+	(pos (getf plist :pos))
+	(keys-lemmas (getf plist :keys))
+	(gloss (getf plist :gloss)))
+    (yason:encode
+     (alexandria:alist-hash-table
+      `(("ofs" . ,ofs)
+	("pos" . ,pos)
+	("keys" . ,(mapcar #'car keys-lemmas))
+	("lemmas" . ,(mapcar #'cdr keys-lemmas))
+	("gloss" . ,gloss))
+      :test 'equal)
+     out)
+    (terpri out)))
+
+(defun sentence->json (plist out)
+  (let ((ofs (read-from-string (getf plist :ofs)))
+	(pos (getf plist :pos))
+	(tokens (getf plist :tokens))
+	(gloss (getf plist :gloss)))
+    (yason:encode
+     (alexandria:alist-hash-table
+      `(("meta" . ,(alexandria:alist-hash-table
+		    `(("ofs" . ,ofs)
+		      ("pos" . ,pos)) :test 'equal))
+	("tokens" . ,(mapcar #'token->json tokens))
+	("raw_text" . ,gloss))
+      :test 'equal)
+     out)
+    (terpri out)))
+
+(defun symbol->string (sym)
+  (string-downcase (string sym)))
+
+(defun token->json (plist)
+  (let ((h (alexandria:plist-hash-table plist))
+	(kind (getf plist :kind)))
+    (alexandria:alist-hash-table
+      `(("form" . ,(gethash :form h))
+	("lemma" . ,(gethash :lemma h))
+	("kind" . ,(if (consp kind)
+		       (alexandria:alist-hash-table (list (cons (symbol->string (car kind))
+								(cdr kind))))
+		       (symbol->string kind)))
+	("senses" . ,(mapcar #'car (gethash :senses h)))
+	("unsure" . ,(gethash :unsure h))
+	("tag" . ,(symbol->string (gethash :tag h "ignore")))
+	("meta" . ,(alexandria:alist-hash-table
+		    (loop
+		      for key being the hash-keys of h
+		      for val being the hash-values of h
+		      unless (member key '(:form :lemma :kind :senses :unsure :tag))
+		      collect (cons (symbol->string key)
+				    (if (symbolp val)
+					(symbol->string val)
+					val))))))
+      :test 'equal)))
