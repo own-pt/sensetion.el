65b0d0b68a13e0efafb81fa444bb72dc569911cf
odanoburu <bcclaro@gmail.com>
2019-09-19 14:04:52 -0300

    * deprecate elasticsearch backend

--- sensetion-client.el
+++ sensetion-client.el
@@ -67,139 +67,6 @@
   (sensetion--backend-lemma->synsets (sensetion--project-backend sensetion-current-project) lemma pos))
 
 
-;;; elasticsearch backend
-(defvar sensetion--es-headers '(("Content-Type" . "application/json ; charset=UTF-8")))
-
-(defvar sensetion--es-size-params '(("size" . "10000")))
-
-(defsubst sensetion--json-read ()
-  (decode-coding-region (point-min) (point-max) 'utf-8)
-  (let ((json-array-type 'list))
-    (json-read)))
-
-(cl-defun sensetion--es-request-debug-fn (&key data error-thrown &allow-other-keys)
-  (let ((inhibit-message t))
-    (message "%s" (json-encode-alist data)))
-  (message "Done %s "
-	   (cl-case (map-elt data 'errors 'unsure #'eq)
-	     (:json-false "with no errors")
-	     (t  "with errors"))))
-
-
-(cl-defun sensetion--es-request (path &key data type params (sync t) debug)
-  (let* ((response (request (format "%s:%s/%s" sensetion-backend-url sensetion-backend-port path)
-			    :headers sensetion--es-headers :parser #'sensetion--json-read
-			    :params params :type type
-			    :sync t :data data :complete (when debug #'sensetion--es-request-debug-fn)))
-	 (data (request-response-data response)))
-    (if-let ((error? (map-elt data 'error)))
-	(error "Elasticsearch error %s" error?)
-      data)))
-
-
-(cl-defun sensetion--es-query (path data &key (type "GET") params (sync t) debug)
-  (let* ((data (sensetion--es-request path :data data :type type :params params
-			     :sync sync :debug debug))
-	 (hits (map-elt (map-elt data 'hits nil #'eq) 'hits nil #'eq))
-	 (docs (mapcar (lambda (hit) (map-elt hit '_source)) hits)))
-    docs))
-
-
-(cl-defmethod sensetion-backend-prefix-lemma ((backend (eql es)) prefix)
-  (let* ((query `((query
-		   (prefix
-		    (terms . ,prefix)))))
-	 (query (json-encode-alist query))
-	 (hits  (sensetion--es-query "sensetion-synsets/_search"
-			    query
-			    :params sensetion--es-size-params))
-	 (terms (seq-mapcat (lambda (doc) (map-elt doc 'terms)) hits)))
-    (seq-filter (lambda (lemma) (string-prefix-p prefix lemma t)) terms)))
-
-
-(cl-defmethod sensetion-backend-prefix-document-id ((backend (eql es)) prefix)
-  (let* ((query `((query
-		   (prefix
-		    (doc_id . ,prefix)))))
-	 (query (json-encode-alist query))
-	 (hits  (sensetion--es-query "sensetion-docs/_search"
-			    query
-			    :params sensetion--es-size-params))
-	 (document-ids (cl-map 'list (lambda (doc) (map-elt doc 'doc_id)) hits)))
-    (seq-filter (lambda (document-id) (string-prefix-p prefix document-id)) document-ids)))
-
-
-(cl-defmethod sensetion--backend-lemma->synsets ((backend (eql es)) lemma pos)
-  (let* ((query `((query
-		   (bool
-		    (filter .
-			    [((term
-			       (terms . ,lemma)))
-			     ((term
-			       (pos . ,pos)))])))))
-	 (query (json-encode-alist query))
-	 (hits (sensetion--es-query "sensetion-synsets/_search"
-			   query
-			   :params sensetion--es-size-params)))
-    (mapcar #'sensetion--alist->synset hits)))
-
-
-(cl-defmethod sensetion--backend-id->sent ((backend (eql es)) sent-id)
-  (map-elt
-   (sensetion--es-request (format "sensetion-docs/_doc/%s" sent-id))
-   '_source nil #'eq))
-
-
-(cl-defmethod sensetion--backend-get-sents ((backend (eql es)) lemma &optional pos)
-  (let* ((lemma (cl-substitute ?_ (string-to-char " ") lemma :test #'eq))
-	 (docs (if pos
-		   (sensetion--es-lemma-pos->docs lemma pos)
-		 (sensetion--es-lemma->docs lemma))))
-    (mapcar #'sensetion--alist->sent docs)))
-
-
-(defun sensetion--es-lemma-pos->docs (lemma pos)
-  (let* ((query `((query
-		   (nested
-		    (path . "tokens")
-		    (query
-		     (regexp
-		      (tokens\.lemmas . ,(format "%s(%%%s)?" lemma (sensetion--pos->synset-type pos)))))))
-		  (sort . ("doc_id" "sent_id"))))
-	 (query (json-encode-alist query))
-	 (hits  (sensetion--es-query "sensetion-docs/_search"
-			    query
-			    :params sensetion--es-size-params)))
-    hits))
-
-
-(defun sensetion--es-lemma->docs (lemma)
-  (let* ((query `((query
-		   (nested
-		    (path . "tokens")
-		    (query
-		     (regexp
-		      (tokens\.lemmas . ,(format "%s(%%[1-4])?" lemma))))))
-		  (sort . ("doc_id" "sent_id"))))
-	 (query (json-encode-alist query))
-	 (hits (sensetion--es-query "sensetion-docs/_search"
-			   query
-			   :params sensetion--es-size-params)))
-    hits))
-
-
-(cl-defmethod sensetion--backend-get-sorted-doc-sents ((backend (eql es)) doc-id)
-  (let* ((query `((query
-		   (term
-		    (doc_id . ,doc-id)))
-		  (sort . ("sent_id"))))
-	 (query (json-encode-alist query))
-	 (hits (sensetion--es-query "sensetion-docs/_search"
-			   query
-			   :params sensetion--es-size-params)))
-    (mapcar #'sensetion--alist->sent hits)))
-
-
 (defun sensetion--remove-man-now (sent)
   (cl-labels
       ((remove-man-now (tk)
@@ -214,12 +81,8 @@
     sent))
 
 
-(cl-defmethod sensetion--backend-update-modified-sent ((backend (eql es)) sent)
-  (let ((data (encode-coding-string (json-encode-alist (sensetion--sent->alist sent)) 'utf-8 t)))
-    (sensetion--es-request (format "sensetion-docs/_doc/%s" (sensetion--sent-id sent))
-		  ;; DISCUSS: could be made async, but then might have
-		  ;; race condition?
-		  :data data :type "PUT")))
+;;; elasticsearch backend deprecated
+
 
 ;;; mongodb backend
 
